370f1fc8c9e1a1a32511622a7dfe084db7a214da
User Name <user@email.com>
2025-10-25 03:22:33 -0400

    Remove incorrect link tag loading JS file as stylesheet

--- resources/views/publisher/auth/register.blade.php
+++ resources/views/publisher/auth/register.blade.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
     <link rel="stylesheet" href="{{ asset('assets/css/publisherregister.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/js/publisherregisterform.js') }}">
 
     <style>
         select.uptitle-input-text {
