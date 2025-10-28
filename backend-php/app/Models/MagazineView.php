0ec86d390d89686070a26e2e99841ad2da8d50ec
User Name <user@email.com>
2025-10-28 00:17:58 -0400

    Add MagazineView model and migration for tracking magazine views

---
+++ backend-php/app/Models/MagazineView.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MagazineView extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'magazine_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function magazine()
+    {
+        return $this->belongsTo(Magazine::class);
+    }
+}
